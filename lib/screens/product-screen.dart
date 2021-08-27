9c4d0c0d72ceaaafdde75a3f8d7c4186623c6510
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-08-26 21:52:22 -0500

    Continuation of product edit screen

--- lib/screens/product-screen.dart
+++ lib/screens/product-screen.dart
@@ -1,3 +1,4 @@
+import 'package:app_products/ui/inputDecorations-ui.dart';
 import 'package:app_products/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -11,12 +12,85 @@ class ProductScreen extends StatelessWidget {
            children: [
              Stack(
                 children: [
-                  ProductImage()
+                  ProductImage(),
+                  Positioned(
+                    left: 20,
+                    top: 40,
+                    child: IconButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      icon: Icon(Icons.arrow_back_ios_outlined, size:40, color: Colors.black),
+                    ),
+                  ),
+                  Positioned(
+                    right: 20,
+                    top: 40,
+                    child: IconButton(
+                      onPressed: (){
+
+                      },
+                      icon: Icon(Icons.camera_alt_outlined, size:40, color: Colors.black),
+                    ),
+                  ),
                 ],
              ),
+             _ProductForm(),
+             SizedBox(height: 100.0,)
            ],
            ),
+         ),
+         floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+         floatingActionButton: FloatingActionButton(
+           child: Icon(Icons.save_outlined),
+           onPressed: (){},
          ),
        );
    }
  }
+
+class _ProductForm extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.0),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(25.0), bottomRight: Radius.circular(25.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.5),
+              offset: Offset(0,5),
+              blurRadius: 5
+            )
+          ]
+        ),
+        child: Form(
+          child: Column(
+            children: [
+              SizedBox(height: 10.0),
+              TextField(
+                decoration: InputDecorations.authInputDecoration(hint_text: 'Nombre del producto', label_text: 'Nombre:'),
+              ),
+              SizedBox(height: 10.0),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecorations.authInputDecoration(hint_text: '\$100', label_text: 'Precio:'),
+              ),
+              SizedBox(height: 10.0),
+              SwitchListTile.adaptive(
+                title: Text('Disponible:'),
+                value: true, 
+                activeColor: Colors.indigo,
+                onChanged: (value){}
+              ),
+              SizedBox(height: 30.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/productImg-widget.dart
+++ lib/widgets/productImg-widget.dart
@@ -4,27 +4,30 @@ class ProductImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 450,
-      child: ClipRRect(
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(45.0), bottomRight: Radius.circular(45.0)),
-        child: FadeInImage(
-          placeholder: AssetImage('assets/jar-loading.gif'),
-          image: NetworkImage('https://pizzerialacosanostra.com/wp-content/uploads/2020/06/01-BOTE-COCACOLA.jpg'),
-          fit: BoxFit.cover,
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        width: double.infinity,
+        height: 450,
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(45.0), topRight: Radius.circular(45.0)),
+          child: FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage('https://pizzerialacosanostra.com/wp-content/uploads/2020/06/01-BOTE-COCACOLA.jpg'),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      decoration: BoxDecoration(
-        color: Colors.red,
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(45.0), bottomRight: Radius.circular(45.0)),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.05),
-            blurRadius: 10,
-            offset: Offset(0,5)
-          )
-        ]
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(45.0), topRight: Radius.circular(45.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.05),
+              blurRadius: 10,
+              offset: Offset(0,5)
+            )
+          ]
+        ),
       ),
     );
   }
