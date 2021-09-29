e010fa0f6d747798a7fabe305bcd964af37fd320
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-09-29 09:33:34 -0500

    Upload Firebase Products

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -12,6 +12,7 @@ class Product {
   String name;
   String? picture;
   double price;
+  String? id;
 
   factory Product.fromJson(String str) => Product.fromMap(json.decode(str));
 
--- lib/services/product_service.dart
+++ lib/services/product_service.dart
@@ -1,8 +1,28 @@
+import 'dart:convert';
+
 import 'package:app_products/models/models.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ProductService extends ChangeNotifier{
   final String _baseUrl = "https://flutter-63eaf-default-rtdb.firebaseio.com";
   final List<Product> products = [];
+  bool isLoading = true;
+
+  ProductService(){
+    this.loadProduct();
+  }
+
+  Future loadProduct()async{
+    final url = Uri.https(this._baseUrl, 'products.json');
+    final resp = await http.get(url);
+    final Map<String, dynamic> productsMap = json.decode(resp.body);
+
+    productsMap.forEach((key, value) {
+      final tempProduct = Product.fromMap(value);
+      tempProduct.id = key;
+      this.products.add(tempProduct);
+    });
+  }
   
 }
